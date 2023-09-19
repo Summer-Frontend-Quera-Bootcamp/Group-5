5127ff852cec99db423657029414572da768646b
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-09-19 20:21:03 +0330

    chore: extend default theme
    added custom styles for both chakra ui and tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,15 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-
-function ChakraRTLProvider({ children }: { children?: ReactNode }) {
-	const theme = extendTheme({ direction: "rtl" });
-	return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
-}
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<ChakraRTLProvider>
+		<ChakraProvider theme={theme}>
 			<App />
-		</ChakraRTLProvider>
+		</ChakraProvider>
 	</React.StrictMode>
 );
 
