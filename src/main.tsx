f8daf67a94dd085286a6e723ff050cdb494ff630
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-09-19 16:45:56 +0330

    setup: install packages and setup configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+function ChakraRTLProvider({ children }: { children?: ReactNode }) {
+	const theme = extendTheme({ direction: "rtl" });
+	return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<ChakraRTLProvider>
+			<App />
+		</ChakraRTLProvider>
 	</React.StrictMode>
 );
 
