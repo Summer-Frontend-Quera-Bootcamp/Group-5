01922d8318f079d56dba9e93a505a945e3202aaf
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-09-20 13:14:58 +0330

    feat(authentication): create page layout

--- src/layouts/Authentication/index.tsx
+++ src/layouts/Authentication/index.tsx
@@ -1,5 +1,48 @@
+import { Box, Button, Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+
 const AuthenticationLayout = () => {
-	return <>authentication</>;
+	return (
+		<Container
+			maxW={[
+				"container.sm",
+				"container.sm",
+				"container.md",
+				"container.lg",
+				"container.xl",
+				"1536px",
+			]}
+		>
+			<Flex align="center" justifyContent="space-between" mt="l">
+				<Heading
+					bgGradient="linear(to-r, #06846F, #54BEE8)"
+					bgClip="text"
+					fill="transparent"
+					display="inline-block"
+					py="2"
+				>
+					کوئرا تسک منیجر
+				</Heading>
+				<Flex align="center" gap="s">
+					<Text fontWeight="semibold">ثبت نام نکرده ای؟</Text>
+					<Button>ثبت نام</Button>
+				</Flex>
+			</Flex>
+			<Box pos="fixed" inset="0" zIndex="-1" display="grid" placeItems="center">
+				<Outlet />
+			</Box>
+			<Box
+				w="200%"
+				h="100%"
+				zIndex="-2"
+				bgGradient="linear(to-l, #06846F, #54BEE8)"
+				pos="fixed"
+				bottom="0"
+				transform="translateX(50%)"
+				clipPath="polygon(100% 100%, -20% 100%, 120% 10%)"
+			/>
+		</Container>
+	);
 };
 
 export default AuthenticationLayout;
