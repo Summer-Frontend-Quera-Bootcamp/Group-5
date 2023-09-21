c51dee08f73cb9f4f33c50d0fe58ba034ea72907
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-09-21 20:57:11 +0330

    feat(pages): create form layout for auth pages
    these are just mock layout which should be replaced

--- src/pages/Reset/index.tsx
+++ src/pages/Reset/index.tsx
@@ -1,5 +1,48 @@
-const ResetPage = () => {
-	return <>reset page</>;
+import { Heading, chakra } from "@chakra-ui/react";
+import { ValidateInput } from "../../components";
+import MyComponent from "../../components/‌Button";
+import { useForm } from "react-hook-form";
+
+const authenticationFormStyle = {
+	display: "flex",
+	flexDir: "column",
+	gap: "32px",
+	bg: "white",
+	shadow: "0px 50px 100px -20px #32325D40, 0px 30px 60px -30px #0000004D",
+	width: "640px",
+	borderRadius: "20px",
+	p: "24px",
+	textAlign: "center",
 };
 
-export default ResetPage;
+const ForgotPage = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	return (
+		<chakra.form sx={authenticationFormStyle}>
+			<Heading>بازیابی رمز عبور</Heading>
+			<ValidateInput
+				type="password"
+				lable="رمز عبور جدید را وارد کنید"
+				// TODO: errors={}
+				// TODO: register={}
+				errors={errors}
+				register={register}
+			/>
+			<MyComponent
+				// TODO: set state for isActive={false} prop
+				isActive={false}
+				// TODO: set type for button
+				onClick={() => {}}
+				fullWidth={true}
+			>
+				تغییر رمز عبور
+			</MyComponent>
+		</chakra.form>
+	);
+};
+
+export default ForgotPage;
