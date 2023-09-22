2c16b20da295b5bc7e0d0e6543b42a771410f81b
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-09-22 16:43:46 +0330

    feat(forgot): add reset email message

--- src/pages/Forgot/index.tsx
+++ src/pages/Forgot/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, chakra } from "@chakra-ui/react";
+import { Heading, Text, chakra } from "@chakra-ui/react";
 import { ValidateInput, Button } from "../../components";
 import { useForm } from "react-hook-form";
 import { authenticationFormStyle } from "../../styles";
@@ -9,25 +9,37 @@ const ForgotPage = () => {
 		handleSubmit,
 		formState: { errors },
 	} = useForm();
-	const onSubmit = (data:any) => console.log(data)
+	const onSubmit = (data: any) => console.log(data);
 	return (
 		<chakra.form sx={authenticationFormStyle} onSubmit={handleSubmit(onSubmit)}>
 			<Heading>فراموشی رمز عبور</Heading>
-			<ValidateInput
-				type="email"
-				lable="ایمیل"
-				errors={errors}
-				register={register}
-			/>
-			<Button
-				// TODO: set state for isActive={false} prop
-				isActive={false}
-				// TODO: set type for button
-				onClick={() => {}}
-				fullWidth={true}
-			>
-				دریافت ایمیل بازیابی رمز عبور
-			</Button>
+			// TODO: change onSubmit
+			{true ? (
+				<>
+					<ValidateInput
+						type="email"
+						lable="ایمیل"
+						errors={errors}
+						register={register}
+					/>
+					<Button
+						// TODO: set state for isActive={false} prop
+						isActive={false}
+						// TODO: set type for button
+						onClick={() => {}}
+						fullWidth={true}
+					>
+						دریافت ایمیل بازیابی رمز عبور
+					</Button>
+				</>
+			) : (
+				<>
+					<Text>
+						لینک بازیابی رمز عبور برای شما ایمیل شد. لطفا ایمیل خود را بررسی
+						کنید.
+					</Text>
+				</>
+			)}
 		</chakra.form>
 	);
 };
