64da008bfe1461fbafb9bfecbaf593ca3a2bfcc6
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-09-22 12:12:52 +0330

    feat: add links to pages

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,32 +1,37 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { Link as ChakraLink } from '@chakra-ui/react';
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
 
 interface LinkProps {
-  href: string;
-  children: React.ReactNode;
-  color?: string;
-  fontSize?: string;
+	href: string;
+	children: React.ReactNode;
+	color?: string;
+	fontSize?: string;
 }
 
-const Link: React.FC<LinkProps> = ({ href, children, color = 'teal.500', fontSize }) => {
-  const linkStyles = {
-    textDecoration: 'none',
-    fontWeight: 'bold',
-    fontSize: fontSize || 'inherit', // Use the provided fontSize or default to 'inherit'
-  };
+const Link: React.FC<LinkProps> = ({
+	href,
+	children,
+	color = "teal.500",
+	fontSize,
+}) => {
+	const linkStyles = {
+		textDecoration: "none",
+		fontWeight: "bold",
+		fontSize: fontSize || "inherit", // Use the provided fontSize or default to 'inherit'
+	};
 
-  return (
-    <ChakraLink
-      as={RouterLink}
-      href={href}
-      color={color}
-      colorScheme="teal.500"
-      style={linkStyles}
-    >
-      {children}
-    </ChakraLink>
-  );
+	return (
+		<ChakraLink
+			as={RouterLink}
+			to={href}
+			color={color}
+			colorScheme="teal.500"
+			sx={linkStyles}
+		>
+			{children}
+		</ChakraLink>
+	);
 };
 
 export default Link;
