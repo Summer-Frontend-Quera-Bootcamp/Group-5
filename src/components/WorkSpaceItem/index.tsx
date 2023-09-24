b24e0a295449d67744885d2c2b90dd729475dfdf
amin hadadi <amin.haddadi1995@gmail.com>
2023-09-25 00:45:24 +0330

    feat: workspaceitem-component

--- src/components/WorkSpaceItem/index.tsx
+++ src/components/WorkSpaceItem/index.tsx
@@ -1,5 +1,26 @@
-const WorkSpaceItem = () => {
-	return <div>WorkSpaceItem</div>;
+import { Box, Flex,Spacer,Stack, Text  } from "@chakra-ui/react";
+
+interface IWorkSpaceItemProps{
+	content:string;
+	color:string;
+}
+
+const WorkSpaceItem = ({content,color}:IWorkSpaceItemProps):JSX.Element => {
+
+	return (
+		<>
+		<Box bg='grey' borderRadius='4px' w='274px' h='36px' p='6px' top='77px' left='20px'>
+			<Flex>
+  				<Stack direction='row' h='28px' w='128px' spacing='4px'>
+				  	<Box bg={color} w='20px' h='20px' borderRadius='4px'></Box>
+					<Text w='100px' h='28px' fontSize='16px' color='black' fontWeight='500' align='right'>{content}</Text>
+  				</Stack >
+  			<Spacer />
+  				{/* here should be an essensial/dots-menu component */}
+			</Flex>
+		</Box>
+		</>
+	);
 };
 
 export default WorkSpaceItem;
