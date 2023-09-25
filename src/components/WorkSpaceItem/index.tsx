7e5156558d728ff98c38c12bbd7ac040cfbac5d1
amin hadadi <amin.haddadi1995@gmail.com>
2023-09-25 23:57:58 +0330

    refactor: workspace-component

--- src/components/WorkSpaceItem/index.tsx
+++ src/components/WorkSpaceItem/index.tsx
@@ -1,24 +1,36 @@
-import { Box, Flex,Spacer,Stack, Text  } from "@chakra-ui/react";
+import {
+	Box,
+	Stack,
+	Text,
+	Accordion,
+	AccordionItem,
+	AccordionButton,
+	AccordionPanel,
+} from "@chakra-ui/react";
 
-interface IWorkSpaceItemProps{
-	content:string;
-	color:string;
+interface IWorkSpaceItemProps {
+	content: string;
+	color: string;
 }
 
-const WorkSpaceItem = ({content,color}:IWorkSpaceItemProps):JSX.Element => {
+const WorkSpaceItem = ({ content, color }: IWorkSpaceItemProps): JSX.Element => {
 
 	return (
 		<>
-		<Box bg='grey' borderRadius='4px' w='274px' h='36px' p='6px' top='77px' left='20px'>
-			<Flex>
-  				<Stack direction='row' h='28px' w='128px' spacing='4px'>
-				  	<Box bg={color} w='20px' h='20px' borderRadius='4px'></Box>
-					<Text w='100px' h='28px' fontSize='16px' color='black' fontWeight='500' align='right'>{content}</Text>
-  				</Stack >
-  			<Spacer />
-  				{/* here should be an essensial/dots-menu component */}
-			</Flex>
-		</Box>
+			<Accordion bg='white' borderRadius='4px' w='274px' h='36px' p='6px' top='77px' left='20px' allowToggle>
+				<AccordionItem>
+					<AccordionButton>
+						<Stack direction='row' h='28px' w='128px' spacing='4px' flex='1'>
+							<Box bg={color} w='20px' h='20px' borderRadius='4px'></Box>
+							<Text w='100px' h='28px' fontSize='16px' color='black' fontWeight='500' align='left'>{content}</Text>
+						</Stack >
+						<Box as="button" w='20px' h='20px'>...</Box>
+					</AccordionButton>
+					<AccordionPanel>
+						todo ProjectItem
+					</AccordionPanel>
+				</AccordionItem>
+			</Accordion>
 		</>
 	);
 };
