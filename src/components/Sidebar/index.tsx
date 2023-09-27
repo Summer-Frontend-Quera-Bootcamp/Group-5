c948829b0df9a4190482e98230f711c0362fa5a0
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-09-27 20:13:08 +0330

    refactor(sidebar): fix sidebar height and width

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,10 @@
-import {  
+import {
 	Heading,
 	Accordion,
 	AccordionItem,
 	AccordionButton,
 	AccordionPanel,
-	AccordionIcon, 
+	AccordionIcon,
 	Text,
 	Button,
 	Box,
@@ -14,56 +14,110 @@ import {
 	Flex,
 	Switch,
 } from "@chakra-ui/react";
-import { SmallAddIcon,ArrowForwardIcon} from '@chakra-ui/icons'
-import { IconButton } from '@chakra-ui/react'
-import {headingStyle} from "../../layouts/Authentication/style";
+import { SmallAddIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+import { IconButton } from "@chakra-ui/react";
+import { headingStyle } from "../../layouts/Authentication/style";
 
-interface ISidebarProp{
-	items?:any[];
-	name:string;
-	src?:string
+interface ISidebarProp {
+	items?: any[];
+	name: string;
+	src?: string;
 }
-const Sidebar = ({items,name,src}:ISidebarProp):JSX.Element => {
+const Sidebar = ({ items, name, src }: ISidebarProp): JSX.Element => {
 	return (
-	<>
-		<Heading sx={headingStyle}>کوئرا تسک منیجر</Heading>
+		<Flex flexDir="column" h="full">
+			<Heading sx={headingStyle}>کوئرا تسک منیجر</Heading>
 
-		<Box w='274px' top='122px' h='430px' left='16px' gap='8px'>
-			<Accordion w='274px' allowToggle>
-  				<AccordionItem>
-      				<AccordionButton>
-        				<Text w='98px' h='28px' flex='1' align='right' fontWeight='800' fontSize='16px' lineHeight='27.64px'>
-          					ورک اسپیس ها
-        				</Text>
-	        		<AccordionIcon />
-    	  	</AccordionButton>
-    		<AccordionPanel pb={4}>
-			<Button leftIcon={<IconButton size='5px'  variant='outline' colorScheme='black' icon={<SmallAddIcon/>} aria-label="space btn"/>} colorScheme='gray' w='255px' h='32px' borderRadius='6px' p='10px' gap='4px' variant='solid'>
-   				<Text mr='-6px' w='110px' h='21px' fontWeight='400px' lineHeight='20.73px' fontSize='12px'>ساختن اسپیس جدید</Text>
-			</Button>
-      			{items && items.map(x=>x)}
-    		</AccordionPanel>
-  			</AccordionItem>
-			</Accordion>
-		</Box>
-
-		<Box top='903px' m='0 32px' w='276px' h='89px' gap='4px'>
-			<Stack direction='row' h='37px' w='144px' spacing='4px'>
-				<Avatar size='sm' name={name} src={src} />
-				<Text pt='4px' w='100px' h='28px' fontSize='16px' color='black' fontWeight='500px' align='right' lineHeight='27.64px'>{name}</Text>
-			</Stack >
-			<Box p='0 xs'>
-				<Flex>
-					<Button leftIcon={<ArrowForwardIcon />} w='64px' h='28px' gap='4px' borderColor='white' color='#818181' colorScheme='teal' variant='outline'>
-    					خروج
-  					</Button>
-				<Spacer/>
-  					<Switch w='64px' h='36px' mt='3px' isInvalid/>
-				</Flex>
+			<Box gap="8px">
+				<Accordion w="274px" allowToggle>
+					<AccordionItem>
+						<AccordionButton>
+							<Text
+								w="98px"
+								h="28px"
+								flex="1"
+								align="right"
+								fontWeight="800"
+								fontSize="16px"
+								lineHeight="27.64px"
+							>
+								ورک اسپیس ها
+							</Text>
+							<AccordionIcon />
+						</AccordionButton>
+						<AccordionPanel pb={4}>
+							<Button
+								leftIcon={
+									<IconButton
+										size="5px"
+										variant="outline"
+										colorScheme="black"
+										icon={<SmallAddIcon />}
+										aria-label="space btn"
+									/>
+								}
+								colorScheme="gray"
+								w="255px"
+								h="32px"
+								borderRadius="6px"
+								p="10px"
+								gap="4px"
+								variant="solid"
+							>
+								<Text
+									mr="-6px"
+									w="110px"
+									h="21px"
+									fontWeight="400px"
+									lineHeight="20.73px"
+									fontSize="12px"
+								>
+									ساختن اسپیس جدید
+								</Text>
+							</Button>
+							{items && items.map((x) => x)}
+						</AccordionPanel>
+					</AccordionItem>
+				</Accordion>
 			</Box>
-		</Box>
-	</>
-	)
+
+			<Box w="full" h="50px" gap="4px" mt="auto">
+				<Stack direction="row" h="37px" w="144px" spacing="4px">
+					<Avatar size="sm" name={name} src={src} />
+					<Text
+						pt="4px"
+						w="100px"
+						h="28px"
+						fontSize="16px"
+						color="black"
+						fontWeight="500px"
+						align="right"
+						lineHeight="27.64px"
+					>
+						{name}
+					</Text>
+				</Stack>
+				<Box p="0 xs">
+					<Flex>
+						<Button
+							leftIcon={<ArrowForwardIcon />}
+							w="64px"
+							h="28px"
+							gap="4px"
+							borderColor="white"
+							color="#818181"
+							colorScheme="teal"
+							variant="outline"
+						>
+							خروج
+						</Button>
+						<Spacer />
+						<Switch w="64px" h="36px" mt="3px" isInvalid />
+					</Flex>
+				</Box>
+			</Box>
+		</Flex>
+	);
 };
 
 export default Sidebar;
--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,12 +1,13 @@
 import { Flex, Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { Sidebar } from "../../components";
 
 const DashboardLayout = () => {
 	return (
 		<Flex wrap="nowrap">
 			{/* todo: create sidebar */}
 			<Box flex="0 0 340px" borderEnd="1px" ps="xl" pe="md" py="lg" h="100dvh">
-				Sidebar
+				<Sidebar name="amir"/>
 			</Box>
 			<Box
 				flex="1 0 calc(100% - 340px)"
