c63f0420020fd65d78e5751de38f8479c978b1bc
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-09-28 00:05:29 +0330

    fix(project page): add active state and extract styles

---
+++ src/pages/Project/style.ts
@@ -0,0 +1,46 @@
+export const tabListStyle = {
+	borderBottom: "1px",
+	borderColor: "gray.400",
+	pb: "xs",
+	gap: "sm",
+	alignItems: "center",
+};
+
+export const tabStyle = {
+	fontSize: "body-md",
+	gap: "xs",
+	_hover: { color: "cyan-primary" },
+};
+
+export const shareButtonStyle = {
+	fontSize: "body-md",
+	fontWeight: "normal",
+	ms: "auto",
+};
+
+export const tabIndicatorStyle = {
+	height: "2.5px",
+	bg: "cyan-primary",
+	borderRadius: "1px",
+	transform: "translate(0, -2px)",
+};
+
+export const searchFilterWrapperStyle = {
+	borderBottom: "1px",
+	borderColor: "gray.400",
+	py: "xs",
+	gap: "xl",
+	alignItems: "center",
+};
+
+export const searchInputStyle = {
+	border: "none",
+	outline: "none",
+	_focus: { border: "none", boxShadow: "none" },
+};
+
+export const newTaskButtonStyle = {
+	pos: "fixed",
+	bottom: "30px",
+	left: "40px",
+};
