94156a25136d58e58a4472928f782cf930a6c75f
elaheh basiri <basirielahe80@gmail.com>
2023-09-29 22:21:25 +0330

    Priority

--- src/layouts/Authentication/index.tsx
+++ src/layouts/Authentication/index.tsx
@@ -1,46 +1,104 @@
+// import { Box, Container, Flex, Heading, Text} from "@chakra-ui/react";
+// import { Outlet, useLocation, useNavigate } from "react-router-dom";
+// import { PriorityList,Button } from "../../components";
+// import {
+// 	authenticationFormLayoutStyle,
+// 	backgroundGradientStyle,
+// 	headingStyle,
+// } from "./style";
+
+// const AuthenticationLayout = () => {
+// 	const location = useLocation();
+// 	const navigate = useNavigate();
+
+// 	const getHeaderHelper = () => {
+// 		const [text, path, buttonText] =
+// 			location.pathname === "/auth/login"
+// 				? ["ثبت‌نام نکرده ای؟", "/auth/register", "ثبت‌نام"]
+// 				: ["قبلا ثبت‌نام کرده‌ای؟", "/auth/login", "ورود"];
+
+// 		return (
+// 			<>
+// 				<Text>{text}</Text>
+// 				<Button isActive={true} onClick={() => navigate(path)}>
+// 					{buttonText}
+// 				</Button>
+// 				<PriorityList name="" flag="" />
+
+// 			</>
+// 		);
+
+
+
+
+
+	
+// 	};
+
+// 	return (
+// 		<Container maxW={"container.xl"}>
+// 			<Flex align="center" justifyContent="space-between" mt="lg">
+// 				<Heading sx={headingStyle}>کوئرا تسک منیجر</Heading>
+// 				<Flex align="center" gap="sm">
+// 					{getHeaderHelper()}
+// 				</Flex>
+// 			</Flex>
+// 			<Box sx={authenticationFormLayoutStyle}>
+// 				<Outlet />
+// 			</Box>
+// 			<Box sx={backgroundGradientStyle} />
+// 		</Container>
+
+// 	);
+// };
+
+
+// export default AuthenticationLayout;
 import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { Button } from "../../components";
+import {  Button, PriorityList } from "../../components";
 import {
-	authenticationFormLayoutStyle,
-	backgroundGradientStyle,
-	headingStyle,
+  authenticationFormLayoutStyle,
+  backgroundGradientStyle,
+  headingStyle,
 } from "./style";
 
 const AuthenticationLayout = () => {
-	const location = useLocation();
-	const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-	const getHeaderHelper = () => {
-		const [text, path, buttonText] =
-			location.pathname === "/auth/login"
-				? ["ثبت‌نام نکرده ای؟", "/auth/register", "ثبت‌نام"]
-				: ["قبلا ثبت‌نام کرده‌ای؟", "/auth/login", "ورود"];
+  const getHeaderHelper = () => {
+    const [text, path, buttonText] =
+      location.pathname === "/auth/login"
+        ? ["ثبت‌نام نکرده ای؟", "/auth/register", "ثبت‌نام"]
+        : ["قبلا ثبت‌نام کرده‌ای؟", "/auth/login", "ورود"];
 
-		return (
-			<>
-				<Text>{text}</Text>
-				<Button isActive={true} onClick={() => navigate(path)}>
-					{buttonText}
-				</Button>
-			</>
-		);
-	};
+    return (
+      <>
+        <Text>{text}</Text>
+        <Button isActive={true} onClick={() => navigate(path)}>
+          {buttonText}
+        </Button>
+      </>
+    );
+  };
+  return (
+    <Container maxW={"container.xl"}>
+      <Flex align="center" justifyContent="space-between" mt="lg">
+        <Heading sx={headingStyle}>کوئرا تسک منیجر</Heading>
+        <Flex align="center" gap="sm">
+          {getHeaderHelper()}
+        </Flex>
+      </Flex>
+      <Box sx={authenticationFormLayoutStyle}>
+        <Outlet />      <PriorityList></PriorityList>
 
-	return (
-		<Container maxW={"container.xl"}>
-			<Flex align="center" justifyContent="space-between" mt="lg">
-				<Heading sx={headingStyle}>کوئرا تسک منیجر</Heading>
-				<Flex align="center" gap="sm">
-					{getHeaderHelper()}
-				</Flex>
-			</Flex>
-			<Box sx={authenticationFormLayoutStyle}>
-				<Outlet />
-			</Box>
-			<Box sx={backgroundGradientStyle} />
-		</Container>
-	);
+      </Box>
+      <Box sx={backgroundGradientStyle} />
+
+    </Container>
+  );
 };
+
 
 export default AuthenticationLayout;
