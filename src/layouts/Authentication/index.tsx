6dc4ba863011ad2c6d2ca6584bf42519e5d2d7ac
elahehbt <144580528+elahehbt@users.noreply.github.com>
2023-09-30 19:28:01 +0330

    Update Authentication

--- src/layouts/Authentication/index.tsx
+++ src/layouts/Authentication/index.tsx
@@ -1,104 +1,53 @@
-// import { Box, Container, Flex, Heading, Text} from "@chakra-ui/react";
-// import { Outlet, useLocation, useNavigate } from "react-router-dom";
-// import { PriorityList,Button } from "../../components";
-// import {
-// 	authenticationFormLayoutStyle,
-// 	backgroundGradientStyle,
-// 	headingStyle,
-// } from "./style";
+import { Box, Container, Flex, Heading, Text} from "@chakra-ui/react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import {Button } from "../../components";
+import {
+	authenticationFormLayoutStyle,
+	backgroundGradientStyle,
+	headingStyle,
+} from "./style";
 
-// const AuthenticationLayout = () => {
-// 	const location = useLocation();
-// 	const navigate = useNavigate();
+const AuthenticationLayout = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
 
-// 	const getHeaderHelper = () => {
-// 		const [text, path, buttonText] =
-// 			location.pathname === "/auth/login"
-// 				? ["ثبت‌نام نکرده ای؟", "/auth/register", "ثبت‌نام"]
-// 				: ["قبلا ثبت‌نام کرده‌ای؟", "/auth/login", "ورود"];
+	const getHeaderHelper = () => {
+		const [text, path, buttonText] =
+			location.pathname === "/auth/login"
+				? ["ثبت‌نام نکرده ای؟", "/auth/register", "ثبت‌نام"]
+				: ["قبلا ثبت‌نام کرده‌ای؟", "/auth/login", "ورود"];
 
-// 		return (
-// 			<>
-// 				<Text>{text}</Text>
-// 				<Button isActive={true} onClick={() => navigate(path)}>
-// 					{buttonText}
-// 				</Button>
-// 				<PriorityList name="" flag="" />
+		return (
+			<>
+				<Text>{text}</Text>
+				<Button isActive={true} onClick={() => navigate(path)}>
+					{buttonText}
+				</Button>
 
-// 			</>
-// 		);
+			</>
+		);
 
 
 
 
 
 	
-// 	};
+	};
 
-// 	return (
-// 		<Container maxW={"container.xl"}>
-// 			<Flex align="center" justifyContent="space-between" mt="lg">
-// 				<Heading sx={headingStyle}>کوئرا تسک منیجر</Heading>
-// 				<Flex align="center" gap="sm">
-// 					{getHeaderHelper()}
-// 				</Flex>
-// 			</Flex>
-// 			<Box sx={authenticationFormLayoutStyle}>
-// 				<Outlet />
-// 			</Box>
-// 			<Box sx={backgroundGradientStyle} />
-// 		</Container>
+	return (
+		<Container maxW={"container.xl"}>
+			<Flex align="center" justifyContent="space-between" mt="lg">
+				<Heading sx={headingStyle}>کوئرا تسک منیجر</Heading>
+				<Flex align="center" gap="sm">
+					{getHeaderHelper()}
+				</Flex>
+			</Flex>
+			<Box sx={authenticationFormLayoutStyle}>
+				<Outlet />
+			</Box>
+			<Box sx={backgroundGradientStyle} />
+		</Container>
 
-// 	);
-// };
-
-
-// export default AuthenticationLayout;
-import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import {  Button, PriorityList } from "../../components";
-import {
-  authenticationFormLayoutStyle,
-  backgroundGradientStyle,
-  headingStyle,
-} from "./style";
-
-const AuthenticationLayout = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const getHeaderHelper = () => {
-    const [text, path, buttonText] =
-      location.pathname === "/auth/login"
-        ? ["ثبت‌نام نکرده ای؟", "/auth/register", "ثبت‌نام"]
-        : ["قبلا ثبت‌نام کرده‌ای؟", "/auth/login", "ورود"];
-
-    return (
-      <>
-        <Text>{text}</Text>
-        <Button isActive={true} onClick={() => navigate(path)}>
-          {buttonText}
-        </Button>
-      </>
-    );
-  };
-  return (
-    <Container maxW={"container.xl"}>
-      <Flex align="center" justifyContent="space-between" mt="lg">
-        <Heading sx={headingStyle}>کوئرا تسک منیجر</Heading>
-        <Flex align="center" gap="sm">
-          {getHeaderHelper()}
-        </Flex>
-      </Flex>
-      <Box sx={authenticationFormLayoutStyle}>
-        <Outlet />      <PriorityList></PriorityList>
-
-      </Box>
-      <Box sx={backgroundGradientStyle} />
-
-    </Container>
-  );
+	);
 };
 
-
-export default AuthenticationLayout;
