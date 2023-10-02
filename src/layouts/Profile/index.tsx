5aa696ed4b507626b311c846c6c334f66c0562d9
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-10-02 14:23:27 +0330

    feat: create profile layout

--- src/layouts/Profile/index.tsx
+++ src/layouts/Profile/index.tsx
@@ -1,5 +1,35 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { useMemo, useState } from "react";
+import AccountInfo from "./AccountInfo";
+import PersonalInfo from "./PersonalInfo";
+import ProfileSidebar from "./ProfileSidebar";
+import Settings from "./Settings";
+import { pageStyle, sidebarStyle } from "./style";
+
 const ProfileLayout = () => {
-	return <>profile</>;
+	const [activePage, setActivePage] = useState<
+		"personal" | "account" | "settings"
+	>("personal");
+
+	const page = useMemo(() => {
+		switch (activePage) {
+			case "personal":
+				return <PersonalInfo />;
+			case "account":
+				return <AccountInfo />;
+			case "settings":
+				return <Settings />;
+		}
+	}, [activePage]);
+
+	return (
+		<Flex wrap="nowrap">
+			<Box sx={sidebarStyle}>
+				<ProfileSidebar selectedPage={activePage} handleClick={setActivePage} />
+			</Box>
+			<Box sx={pageStyle}>{page}</Box>
+		</Flex>
+	);
 };
 
 export default ProfileLayout;
