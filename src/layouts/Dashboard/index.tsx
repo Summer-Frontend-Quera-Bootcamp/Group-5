bddc9595b5a9a5d48b92b2626be4604fc42ca499
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-10-02 14:27:01 +0330

    refactor(dashboard): extract styles for dashboard layout

--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,21 +1,15 @@
 import { Flex, Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../../components";
+import { mainPageStyle, sidebarStyle } from "./style";
 
 const DashboardLayout = () => {
 	return (
 		<Flex wrap="nowrap">
-			{/* todo: create sidebar */}
-			<Box flex="0 0 340px" borderEnd="1px" ps="xl" pe="md" py="lg" h="100dvh">
-				<Sidebar name="amir"/>
+			<Box sx={sidebarStyle}>
+				<Sidebar name="amir" />
 			</Box>
-			<Box
-				flex="1 0 calc(100% - 340px)"
-				ps="md"
-				pe="xl"
-				py="lg"
-				overflowX="auto"
-			>
+			<Box sx={mainPageStyle}>
 				<Outlet />
 			</Box>
 		</Flex>
