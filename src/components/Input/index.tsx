b3943890bd3c3b7fee689c240dcb55b683da4e1a
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-10-03 17:15:42 +0330

    refactor: format all files and remove unused code

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,56 +1,59 @@
-import  EmailInput  from "./EmailInput";
-import  TextInput  from "./TextInput";
-import  PasswordInput  from "./PasswordInput";
+import EmailInput from "./EmailInput";
+import TextInput from "./TextInput";
+import PasswordInput from "./PasswordInput";
 
-type TInputType= 'password' | 'text' | 'email'
+type TInputType = "password" | "text" | "email";
 interface IInputProps {
-    type:TInputType
+	type: TInputType;
 	label: string;
 	register: any;
 	errors: any;
 	page?: string;
-    placeholder?: string;
+	placeholder?: string;
 }
 
 const ValidateInput = ({
-    type,
-    label,
-    register,
-    errors,
-    page,
-    placeholder
-    }:IInputProps):JSX.Element => {
-        
-    {switch (type) {
-        case "text":
-            return (
-            <TextInput 
-            errors={errors} 
-            register={register} 
-            label={label} 
-            placeholder={placeholder} 
-            />)
+	type,
+	label,
+	register,
+	errors,
+	page,
+	placeholder,
+}: IInputProps): JSX.Element => {
+	{
+		switch (type) {
+			case "text":
+				return (
+					<TextInput
+						errors={errors}
+						register={register}
+						label={label}
+						placeholder={placeholder}
+					/>
+				);
 
-        case "email":
-            return (
-            <EmailInput 
-            errors={errors} 
-            register={register} 
-            label={label} 
-            placeholder={placeholder}
-            />)
+			case "email":
+				return (
+					<EmailInput
+						errors={errors}
+						register={register}
+						label={label}
+						placeholder={placeholder}
+					/>
+				);
 
-        case "password":
-            return (
-            <PasswordInput 
-            errors={errors}  
-            register={register} 
-            label={label} 
-            page={page} 
-            placeholder={placeholder}
-            />)
-            
-    }}
-}
- 
+			case "password":
+				return (
+					<PasswordInput
+						errors={errors}
+						register={register}
+						label={label}
+						page={page}
+						placeholder={placeholder}
+					/>
+				);
+		}
+	}
+};
+
 export default ValidateInput;
--- src/components/‌Button/index.tsx
+++ src/components/‌Button/index.tsx
@@ -1,51 +1,51 @@
-import React, { ReactNode, MouseEvent } from 'react';
-import { Button as ChakraButton, Flex } from '@chakra-ui/react';
-import { Icon } from '@chakra-ui/icons';
+import React, { ReactNode, MouseEvent } from "react";
+import { Button as ChakraButton, Flex } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/icons";
 
 interface ButtonProps {
-  isActive: boolean;
-  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
-  children: ReactNode;
-  icon?: any;
-  fullWidth?: boolean;
-  type?: "button" | "submit" | "reset";
+	isActive: boolean;
+	onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+	children: ReactNode;
+	icon?: any;
+	fullWidth?: boolean;
+	type?: "button" | "submit" | "reset";
 }
 
 const Button: React.FC<ButtonProps> = ({
-  isActive,
-  onClick,
-  children,
-  icon: IconComponent,
-  fullWidth = false,
-  type = "button",
-  ...restProps
+	isActive,
+	onClick,
+	children,
+	icon: IconComponent,
+	fullWidth = false,
+	type = "button",
+	...restProps
 }) => {
-  return (
-    <Flex
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      width={fullWidth ? '100%' : undefined}
-    >
-      <ChakraButton
-        type={type}
-        colorScheme="teal"
-        size="md"
-        _hover={{
-          background: '#006c6c',
-          color: 'white',
-        }}
-        margin="auto"
-        onClick={onClick}
-        disabled={!isActive}
-        {...restProps}
-        width={fullWidth ? '100%' : undefined}
-        leftIcon={IconComponent && <Icon as={IconComponent} />}
-      >
-        {children}
-      </ChakraButton>
-    </Flex>
-  );
+	return (
+		<Flex
+			direction="row"
+			justifyContent="center"
+			alignItems="center"
+			width={fullWidth ? "100%" : undefined}
+		>
+			<ChakraButton
+				type={type}
+				colorScheme="teal"
+				size="md"
+				_hover={{
+					background: "#006c6c",
+					color: "white",
+				}}
+				margin="auto"
+				onClick={onClick}
+				disabled={!isActive}
+				{...restProps}
+				width={fullWidth ? "100%" : undefined}
+				leftIcon={IconComponent && <Icon as={IconComponent} />}
+			>
+				{children}
+			</ChakraButton>
+		</Flex>
+	);
 };
 
 export default Button;
