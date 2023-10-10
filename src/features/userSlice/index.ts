47b11f7975b42c10e84a568efc72b50005aace71
amin hadadi <amin.haddadi1995@gmail.com>
2023-10-10 15:45:45 +0330

    refactor: set auto redirect to dashboard

--- src/features/userSlice/index.ts
+++ src/features/userSlice/index.ts
@@ -1,47 +1,50 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { useEffect } from "react";
 import { AXIOS } from "../../utils/functions/AXIOS";
+import { useNavigate } from "react-router-dom";
 interface IUserstate {
-	firstname: string | null;
-	lastname: string | null;
+	username: string | null;
 	email: string | null;
 	token: string | null;
 	loggedIn: boolean;
 }
 
 let initialState: IUserstate = {
-	firstname: "",
-	lastname: "",
+	username: "",
 	email: "",
 	token: "",
 	loggedIn: false,
 };
+// const navigate = useNavigate();
 
-// useEffect(()=>{
-//     const token=localStorage.getItem('token')
-//     if(token){
-//         const email=localStorage.getItem('email')
-//         if(email){
-//             initialState={
-//                 username:localStorage.getItem('username'),
-//                 email:email,
-//                 token,
-//                 loggedIn:true
-//             }
-//         }
-//         AXIOS.defaults.headers.common.Authorization=`Bearer ${token}`
-//     }
-//     else{
-//         // redirect to login page
-//     }
-// },[])
+// useEffect(() => {
+// 	const token = localStorage.getItem("token");
+// 	if (token) {
+// 		const email = localStorage.getItem("email");
+// 		if (email) {
+// 			initialState = {
+// 				username: localStorage.getItem("username"),
+// 				email: email,
+// 				token,
+// 				loggedIn: true,
+// 			};
+// 		}
+// 		AXIOS.defaults.headers.common.Authorization = `Bearer ${token}`;
+// 		navigate("/dashboard");
+// 	} else {
+// 		navigate("/");
+// 	}
+// }, []);
 
 export const userSlice = createSlice({
 	name: "user",
 	initialState,
 	reducers: {
 		setUser(state, action: PayloadAction<IUserstate>) {
-			state = action.payload;
+			state.username = action.payload.username;
+			state.email = action.payload.email;
+			state.token = action.payload.token;
+			state.loggedIn = action.payload.loggedIn;
 		},
 	},
 });
