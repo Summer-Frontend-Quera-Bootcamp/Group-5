31058bf7eece637e83225a5e1da0d1fdc4128e23
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-10-16 11:20:32 +0330

    fix: set proper background for authentication form

--- src/components/Form/style.ts
+++ src/components/Form/style.ts
@@ -2,7 +2,6 @@ export const authenticationFormStyle = {
 	display: "flex",
 	flexDir: "column",
 	gap: "lg",
-	bg: "white",
 	shadow: "0px 50px 100px -20px #32325D40, 0px 30px 60px -30px #0000004D",
 	width: "640px",
 	borderRadius: "20px",
