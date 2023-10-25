dd3b8b9ad84a8817df120510d57f87255677d0d9
amin hadadi <amin.haddadi1995@gmail.com>
2023-10-25 19:02:43 +0330

    refactor :sidebar-columnmore-component codes

--- src/features/projectSlice/index.ts
+++ src/features/projectSlice/index.ts
@@ -1,12 +1,13 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getAllprojects } from "../../services/api";
+import { AXIOS } from "../../utils/functions/AXIOS";
 
 const initialState: IProject[] = [];
 
 export const setProjectItems = createAsyncThunk(
 	"projects/setProjectItems",
-	(workspaceId) => {
-		const res = getAllprojects(workspaceId);
+	async (workspaceId, thunkAPI) => {
+		const res = await AXIOS.get(`/workspaces/${workspaceId}/projects/`);
 		return res.data;
 	}
 );
