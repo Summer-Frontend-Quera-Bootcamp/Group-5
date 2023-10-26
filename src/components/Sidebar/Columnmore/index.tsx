bde2d3ac792887174dc8e6d9432043f2c1ff3d4e
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-10-26 23:59:19 +0330

    fix: modal close function

--- src/components/Sidebar/Columnmore/index.tsx
+++ src/components/Sidebar/Columnmore/index.tsx
@@ -96,6 +96,7 @@ const ColumnMore = ({
 										<NewWorkSpaceContent
 											type="edit"
 											workSpaseKey={workSpaseKey}
+											onClose={onSecondModalClose}
 										/>
 									</ModalBody>
 								</ModalContent>
